c946a06a6ccfea2d8ac0b9993ca7c294e3d65b9f
hp910219 <792089975@qq.com>
2018-05-14 17:04:51 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/python
+# Create Date 2018/5/14 0014
+__author__ = 'huohuo'
+from setuptools import setup
+setup(
+    name='my_doc',
+    version='0.1',
+    description='generate word',
+    url='',
+    author='Huo huo',
+    author_email='hp910219@126.com',
+    license='MIT'
+)
+
+if __name__ == "__main__":
+    pass
+    
+
